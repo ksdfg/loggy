d6d3d0231b6d3bb53d77e904fad70ee0ad80365b
ksdfg <ksdfg123@gmail.com>
2023-09-18 00:19:06 +0530

    test(combined_handler): add more unit tests
    100% test coverage for CombinedHandler, let's gooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo!
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- combined_handler_test.go
+++ combined_handler_test.go
@@ -1,8 +1,10 @@
 package loggy_test
 
 import (
+	"context"
 	"io"
 	"log/slog"
+	"os"
 	"strings"
 	"testing"
 
@@ -156,4 +158,179 @@ func TestNewCombinedHandler_Error(t *testing.T) {
 		"\n",
 	)
 	assert.Equal(t, expectedOutput, outputStream.String())
+}
+
+// TestNewCombinedHandler_WithAttrs tests the WithAttr method of the CombinedHandler returned by NewCombinedHandler
+// with a string attribute added to the logger that is using the generated CombinedHandler.
+func TestNewCombinedHandler_WithAttrs(t *testing.T) {
+	// Create a strings.Builder to capture the output
+	var outputStream strings.Builder
+
+	// Set up the logger with the combined handler and redirect the output to outputStream
+	setupLoggerWithCombinedHandler(&outputStream)
+
+	// Update default logger with attributes
+	logger := slog.Default().With(slog.String("test_key", "test_value"))
+	slog.SetDefault(logger)
+
+	// Log an error message
+	slog.Error("this is an error log")
+
+	// Check the output
+	expectedOutput := strings.Join(
+		[]string{
+			"level=ERROR msg=\"this is an error log\" logger=debugTextLogHandler test_key=test_value",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"infoJSONLogHandler\",\"test_key\":\"test_value\"}",
+			"level=ERROR msg=\"this is an error log\" logger=warnTextLogHandler test_key=test_value",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"errorJSONLogHandler\",\"test_key\":\"test_value\"}\n",
+		},
+		"\n",
+	)
+	assert.Equal(t, expectedOutput, outputStream.String())
+}
+
+// TestNewCombinedHandler_WithAttrs_EmptyAttrs tests the WithAttr method of the CombinedHandler returned by
+// NewCombinedHandler with an empty attribute list added to the logger that is using the generated CombinedHandler.
+func TestNewCombinedHandler_WithAttrs_EmptyAttrs(t *testing.T) {
+	// Create a strings.Builder to capture the output
+	var outputStream strings.Builder
+
+	// Set up the logger with the combined handler and redirect the output to outputStream
+	setupLoggerWithCombinedHandler(&outputStream)
+
+	// Update default logger with no attributes
+	logger := slog.Default().With()
+	slog.SetDefault(logger)
+
+	// Log an error message
+	slog.Error("this is an error log")
+
+	// Check the output
+	expectedOutput := strings.Join(
+		[]string{
+			"level=ERROR msg=\"this is an error log\" logger=debugTextLogHandler",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"infoJSONLogHandler\"}",
+			"level=ERROR msg=\"this is an error log\" logger=warnTextLogHandler",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"errorJSONLogHandler\"}\n",
+		},
+		"\n",
+	)
+	assert.Equal(t, expectedOutput, outputStream.String())
+}
+
+// TestNewCombinedHandler_WithGroup tests the WithGroup method of the CombinedHandler returned by NewCombinedHandler
+// with a test group added to the logger that is using the generated CombinedHandler.
+func TestNewCombinedHandler_WithGroup(t *testing.T) {
+	// Create a strings.Builder to capture the output
+	var outputStream strings.Builder
+
+	// Set up the logger with the combined handler and redirect the output to outputStream
+	setupLoggerWithCombinedHandler(&outputStream)
+
+	// Update default logger with new group
+	logger := slog.Default().WithGroup("test")
+	slog.SetDefault(logger)
+
+	// Log an error message
+	slog.Error("this is an error log", slog.String("test_key", "test_value"))
+
+	// Check the output
+	expectedOutput := strings.Join(
+		[]string{
+			"level=ERROR msg=\"this is an error log\" logger=debugTextLogHandler test.test_key=test_value",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"infoJSONLogHandler\",\"test\":{\"test_key\":\"test_value\"}}",
+			"level=ERROR msg=\"this is an error log\" logger=warnTextLogHandler test.test_key=test_value",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"errorJSONLogHandler\",\"test\":{\"test_key\":\"test_value\"}}\n",
+		},
+		"\n",
+	)
+	assert.Equal(t, expectedOutput, outputStream.String())
+}
+
+// TestNewCombinedHandler_WithGroup_EmptyName tests the WithGroup method of the CombinedHandler returned by NewCombinedHandler
+// with an empty group added to the logger that is using the generated CombinedHandler.
+func TestNewCombinedHandler_WithGroup_EmptyName(t *testing.T) {
+	// Create a strings.Builder to capture the output
+	var outputStream strings.Builder
+
+	// Set up the logger with the combined handler and redirect the output to outputStream
+	setupLoggerWithCombinedHandler(&outputStream)
+
+	// Update default logger with new group
+	logger := slog.Default().WithGroup("")
+	slog.SetDefault(logger)
+
+	// Log an error message
+	slog.Error("this is an error log", slog.String("test_key", "test_value"))
+
+	// Check the output
+	expectedOutput := strings.Join(
+		[]string{
+			"level=ERROR msg=\"this is an error log\" logger=debugTextLogHandler test_key=test_value",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"infoJSONLogHandler\",\"test_key\":\"test_value\"}",
+			"level=ERROR msg=\"this is an error log\" logger=warnTextLogHandler test_key=test_value",
+			"{\"level\":\"ERROR\",\"msg\":\"this is an error log\",\"logger\":\"errorJSONLogHandler\",\"test_key\":\"test_value\"}\n",
+		},
+		"\n",
+	)
+	assert.Equal(t, expectedOutput, outputStream.String())
+}
+
+// TestNewCombinedHandler_HandleError tests the CombinedHandler returned by NewCombinedHandler with a closed
+// io.Writer passed to one of the handlers passed to NewCombinedHandler.
+func TestNewCombinedHandler_HandleError(t *testing.T) {
+	// Create a strings.Builder to capture the output
+	writer, _, err := os.Pipe()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// Set up the logger with the combined handler and redirect the output to writer generated above
+	handler := loggy.NewCombinedHandler(slog.NewTextHandler(writer, nil))
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+
+	// Close the writer to force an error
+	err = writer.Close()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// Log an error message
+	slog.Error("this is an error log", slog.String("test_key", "test_value"))
+}
+
+// TestNewCombinedHandler_Enabled_False tests the Enabled method of the CombinedHandler returned by NewCombinedHandler
+// which has a WARN and ERROR level TextHandlers and checks if INFO level logs are disabled as expected.
+func TestNewCombinedHandler_Enabled_False(t *testing.T) {
+	// Create new combined handler with loggers for Warn and Error levels
+	handler := loggy.NewCombinedHandler(
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelWarn}),
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError}),
+	)
+
+	// Set up the logger with the combined handler
+	logger := slog.New(handler)
+
+	// Check if Info level is disabled
+	assert.Equal(t, false, logger.Enabled(context.Background(), slog.LevelInfo))
+}
+
+// TestNewCombinedHandler_Enabled_False tests the Enabled method of the CombinedHandler returned by NewCombinedHandler
+// which has an INFO, WARN and ERROR level TextHandlers and checks if INFO level logs are disabled as expected.
+func TestNewCombinedHandler_Enabled_True(t *testing.T) {
+	// Create new combined handler with loggers for Info, Warn and Error levels
+	handler := loggy.NewCombinedHandler(
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelWarn}),
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError}),
+	)
+
+	// Set up the logger with the combined handler
+	logger := slog.New(handler)
+
+	// Check if Info level is enabled
+	assert.Equal(t, true, logger.Enabled(context.Background(), slog.LevelInfo))
 }
