62ac9bdedadec330c2bc82bb02ef306dbc63f821
ksdfg <ksdfg123@gmail.com>
2023-09-17 21:21:13 +0530

    chore: use a different logger for each level in ExampleNewCombinedHandler

--- example_test.go
+++ example_test.go
@@ -9,18 +9,31 @@ import (
 
 func ExampleNewCombinedHandler() {
 	// Create handlers for different log levels
-	jsonLogHandler := slog.NewJSONHandler(os.Stderr, nil)
-	debugLogHandler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})
-	errorLogHandler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError, AddSource: true})
+	debugTextLogHandler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})
+	infoJSONLogHandler := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo})
+	warnTextLogHandler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelWarn, AddSource: true})
+	errorJSONLogHandler := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError, AddSource: true})
 
 	// Add attributes to each handler so that we know which logs came from which handler
-	jsonLogHandlerWithAttr := jsonLogHandler.WithAttrs([]slog.Attr{slog.String("logger", "jsonLogHandler")})
-	debugLogHandlerWithAttr := debugLogHandler.WithAttrs([]slog.Attr{slog.String("logger", "debugLogHandler")})
-	errorLogHandlerWithAttr := errorLogHandler.WithAttrs([]slog.Attr{slog.String("logger", "errorLogHandler")})
+	debugTextLogHandlerWithAttr := debugTextLogHandler.WithAttrs(
+		[]slog.Attr{slog.String("logger", "debugTextLogHandler")},
+	)
+	infoJSONLogHandlerWithAttr := infoJSONLogHandler.WithAttrs(
+		[]slog.Attr{slog.String("logger", "infoJSONLogHandler")},
+	)
+	warnTextLogHandlerWithAttr := warnTextLogHandler.WithAttrs(
+		[]slog.Attr{slog.String("logger", "warnTextLogHandler")},
+	)
+	errorJSONLogHandlerWithAttr := errorJSONLogHandler.WithAttrs(
+		[]slog.Attr{slog.String("logger", "errorJSONLogHandler")},
+	)
 
 	// Create a combined handler with the above handlers
 	combinedHandler := loggy.NewCombinedHandler(
-		jsonLogHandlerWithAttr, debugLogHandlerWithAttr, errorLogHandlerWithAttr,
+		debugTextLogHandlerWithAttr,
+		infoJSONLogHandlerWithAttr,
+		warnTextLogHandlerWithAttr,
+		errorJSONLogHandlerWithAttr,
 	)
 
 	// Create a new logger with the combined handler, and set it as the default
@@ -36,14 +49,16 @@ func ExampleNewCombinedHandler() {
 	// Log an error message
 	slog.Error("this is an error log")
 	// Output:
-	// time=2023-09-17T17:44:01.603+05:30 level=DEBUG msg="this is a debug log" logger=debugLogHandler
-	// {"time":"2023-09-17T17:44:01.603497782+05:30","level":"INFO","msg":"this is an info log","logger":"jsonLogHandler"}
-	// time=2023-09-17T17:44:01.603+05:30 level=INFO msg="this is an info log" logger=debugLogHandler
-	// {"time":"2023-09-17T17:44:01.603500753+05:30","level":"WARN","msg":"this is a warning log","logger":"jsonLogHandler"}
-	// time=2023-09-17T17:44:01.603+05:30 level=WARN msg="this is a warning log" logger=debugLogHandler
-	// {"time":"2023-09-17T17:44:01.603502747+05:30","level":"ERROR","msg":"this is an error log","logger":"jsonLogHandler"}
-	// time=2023-09-17T17:44:01.603+05:30 level=ERROR msg="this is an error log" logger=debugLogHandler
-	// time=2023-09-17T17:44:01.603+05:30 level=ERROR source=/home/ksdfg/code/loggy/example_test.go:37 msg="this is an error log" logger=errorLogHandler
+	// time=2023-09-17T20:01:50.364+05:30 level=DEBUG msg="this is a debug log" logger=debugTextLogHandler
+	// time=2023-09-17T20:01:50.364+05:30 level=INFO msg="this is an info log" logger=debugTextLogHandler
+	// {"time":"2023-09-17T20:01:50.364653675+05:30","level":"INFO","msg":"this is an info log","logger":"infoJSONLogHandler"}
+	// time=2023-09-17T20:01:50.364+05:30 level=WARN msg="this is a warning log" logger=debugTextLogHandler
+	// {"time":"2023-09-17T20:01:50.364658189+05:30","level":"WARN","msg":"this is a warning log","logger":"infoJSONLogHandler"}
+	// time=2023-09-17T20:01:50.364+05:30 level=WARN source=/home/ksdfg/code/loggy/combined_handler_test.go:56 msg="this is a warning log" logger=warnTextLogHandler
+	// time=2023-09-17T20:01:50.364+05:30 level=ERROR msg="this is an error log" logger=debugTextLogHandler
+	// {"time":"2023-09-17T20:01:50.36466392+05:30","level":"ERROR","msg":"this is an error log","logger":"infoJSONLogHandler"}
+	// time=2023-09-17T20:01:50.364+05:30 level=ERROR source=/home/ksdfg/code/loggy/combined_handler_test.go:58 msg="this is an error log" logger=warnTextLogHandler
+	// {"time":"2023-09-17T20:01:50.36466392+05:30","level":"ERROR","source":{"function":"github.com/ksdfg/loggy_test.TestNewCombinedHandler","file":"/home/ksdfg/code/loggy/combined_handler_test.go","line":58},"msg":"this is an error log","logger":"errorJSONLogHandler"}
 }
 
 func ExampleNewConsoleLogHandler() {
